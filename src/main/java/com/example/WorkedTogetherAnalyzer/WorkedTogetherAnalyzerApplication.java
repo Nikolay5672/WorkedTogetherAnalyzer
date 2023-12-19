5a9b6e3768955bd39e0f81f7bf727b772cadaf68
nikolay <nikolay_5672@mail.bg>
2023-12-19 14:04:09 +0200

    initial commit

---
+++ src/main/java/com/example/WorkedTogetherAnalyzer/WorkedTogetherAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.example.WorkedTogetherAnalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkedTogetherAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkedTogetherAnalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/WorkedTogetherAnalyzer/WorkedTogetherAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WorkedTogetherAnalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkedTogetherAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
